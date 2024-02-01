8f3ff65bc3263284594f65f4685b814698c074e1
Roshanking021 <roshanking021@gmail.com>
2024-02-01 14:08:36 +0530

    completed task 1

--- React_V_Assingnment/Fetch/src/App.jsx
+++ React_V_Assingnment/Fetch/src/App.jsx
@@ -1,46 +1,56 @@
-import { useState } from 'react'
-import './App.css'
-import DataDisplay from "./components/DataDisplay"
+import { useState } from "react";
+import "./App.css";
+import DataDisplay from "./components/DataDisplay";
 
 function App() {
-  const [data,setData]=useState([]);
-  const [isLoding,setIsLoading]=useState(false)
-  const [errors,setErrors]=useState(null)
+  const [data, setData] = useState([]);
+  const [isLoding, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState(null);
+  const [isrendered, setIsrenndered] = useState(false);
 
-
-
-  async function fetchData (){
-    setIsLoading(true)
-    setErrors(null)
+  async function fetchData() {
+    setIsLoading(true);
+    setErrors(null);
     try {
+      console.log("inside async func");
       // throw new Error("opps .. Something went wrong...")
-      let res= await fetch('https://jsonplaceholder.typicode.com/posts')
-      let finalRes= await res.json()
-      setData(finalRes)
-      setIsLoading(false)
-      
+      let res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      let finalRes = await res.json();
+      setData(finalRes);
+      setIsLoading(false);
+      setIsrenndered(true);
     } catch (error) {
-      console.log(error.message)
-      setErrors(error.message)
-      setIsLoading(false)
-      
+      console.log(error.message);
+      setErrors(error.message);
+      setIsLoading(false);
     }
   }
 
+  console.log("rendering", Date.now());
+
   return (
     <>
-    <button onClick={fetchData}>Fetch the Data</button>
-    {isLoding && <h4>Loading...</h4>}
-    
-    <div style={{display:"grid",gridTemplateColumns:"repeat(3,1fr)" ,gap:"10px" ,margin:"20px"}}>
-    <DataDisplay data={data}/>
-    </div>
-    {errors && <p>{errors}</p>}
+      {data.length == 0 && <button onClick={fetchData}>Fetch the Data</button>}
+      {isLoding && <h4>Loading...</h4>}
 
+      {data.length === 0 && isrendered ? (
+        <p>Data not Found</p>
+      ) : (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3,1fr)",
+            gap: "40px",
+            margin: "20px",
+          }}
+        >
+          <DataDisplay data={data} />
+        </div>
+      )}
 
-      
+      {errors && <p>{errors}</p>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- React_V_Assingnment/Fetch/src/components/DataDisplay.jsx
+++ React_V_Assingnment/Fetch/src/components/DataDisplay.jsx
@@ -1,12 +1,13 @@
 function DataDisplay({data}){
     return(
         <>{data.map((item)=>
-            <div style={{border:"2px solid black",borderRadius:"10px"}} key={item.id}>
-                <h2>ID: {item.id}</h2>
+            
+            <div id="item" style={{borderRadius:"10px",textAlign:"left",padding:"15px",boxShadow: "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"}} key={item.id}>
+                {item.id%2==0?(<h2 style={{color:"red"}}>ID: {item.id}</h2>):(<h2 style={{color:"Green"}}>ID: {item.id}</h2>)}
                 <h2>Title: {item.title}</h2>
-                <h2>Body: {item.body}</h2>
-
-            </div>)}</>
+                <h3>Body: {item.body}</h3>
+            </div>)}
+            </>
     )
 }
 
