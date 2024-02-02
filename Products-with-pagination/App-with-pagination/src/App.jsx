f5738f0cc31e56bb5d21e6a95ca89e3287f7b2bc
Roshanking021 <roshanking021@gmail.com>
2024-02-02 11:17:01 +0530

    added Previous and Next button

--- Products-with-pagination/App-with-pagination/src/App.jsx
+++ Products-with-pagination/App-with-pagination/src/App.jsx
@@ -1,82 +1,111 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
-import Postitem from './components/Postitem'
+import "./App.css";
+import Postitem from "./components/Postitem";
 
-
-
-
-
-async function getData(url){
+async function getData(url) {
   try {
-    let data=await fetch(url);
-    console.log("ok")
-    let totalItems=Number(data.headers.get("X-Total-Count"));
-    let totalPages=Math.ceil(totalItems/10)
-    let parsedData= await data.json()
+    let data = await fetch(url);
+    console.log("ok");
+    let totalItems = Number(data.headers.get("X-Total-Count"));
+    let totalPages = Math.ceil(totalItems / 10);
+    let parsedData = await data.json();
 
-    return {parsedData:parsedData,totalPages:totalPages}
-
+    return { parsedData: parsedData, totalPages: totalPages };
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-
-
 function App() {
+  const [isLoding, setIsLoading] = useState(false);
+  const [post, setPost] = useState([]);
+  const [errors, setErrors] = useState(null);
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(1);
 
-  const [isLoding,setIsLoading]=useState(false)
-  const [post,setPost]=useState([])
-  const [errors,setErrors] = useState(null)
-  const [page,setPage]=useState(1)
-  const [pageCount,setPageCount]=useState(1)
-
-
-
-  async function FetchandUpdateData(){
-    setIsLoading(true)
-    setErrors(null)
+  async function FetchandUpdateData() {
+    setIsLoading(true);
+    setErrors(null);
     try {
-      // throw new Error("Hi this is a costume error")
-      let {parsedData,totalPages} = await getData(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`)
-      setPost(parsedData)
-      setPageCount(totalPages)
-      setIsLoading(false)
-      
+      // throw new Error("Hi this is a costume error");
+      let { parsedData, totalPages } = await getData(
+        `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
+      );
+      setPost(parsedData);
+      setPageCount(totalPages);
+      setIsLoading(false);
     } catch (error) {
-      setErrors(error.message)
-      setIsLoading(false)
-      
+      setErrors(error.message);
+      setIsLoading(false);
     }
   }
 
+  useEffect(() => {
+    FetchandUpdateData();
+    console.log(pageCount);
+  }, [page]);
 
-    useEffect(()=>{
-      FetchandUpdateData()
-      console.log(pageCount)
-    },[page])
+  function handleprevClick() {
+    page == 1 && alert("There is nothing before this");
 
-console.log(pageCount)
+    page > 1 && setPage(page - 1);
+
+    console.log("alright");
+  }
+
+  function handelnextClick() {
+    page == pageCount && alert("Pages ends here");
+
+    page < pageCount && setPage(page + 1);
+  }
+
+  console.log(pageCount);
+  console.log(page);
   return (
     <>
-    {isLoding && <p>Loading....</p>}
-    {errors && <p>Opps.. Something went wrong</p>}
-    <div style={{display:"grid",gap:"40px", gridTemplateColumns:"repeat(2,1fr)"}}>
-    <Postitem post={post} />
-    </div>
+      {isLoding && <p>Loading....</p>}
+      {errors && <p>Opps.. Something went wrong</p>}
+      <div
+        className="Content-div"
+        style={{
+          display: "grid",
+          maxWidth: "80vw",
+          margin: "0 auto",
+          gap: "40px",
+          gridTemplateColumns: "repeat(3,1fr)",
+          justifyContent: "center",
+        }}
+      >
+        <Postitem post={post} />
+      </div>
 
-    <div style={{display:'flex',marginTop:"50px",maxWidth:"80vw", justifyContent:"center",gap:"25px", marginInline:"auto"}}>
-    {pageCount>1 && new Array(pageCount).fill(0).map((item,index)=>{
-      return(
-        <button style={{border:"2px solid black"}} onClick={()=>setPage(index+1)} key={index}>{index+1}</button>
-      )
-    })}
-    </div>
-
-     
+      <div
+        style={{
+          display: "flex",
+          marginTop: "3rem",
+          maxWidth: "40vw",
+          justifyContent: "center",
+          gap: "25px",
+          marginInline: "auto",
+        }}
+      >
+        {pageCount > 1 && page <= pageCount && (
+          <>
+            <button onClick={handleprevClick}>Prev</button>
+            <button onClick={handelnextClick}>Next</button>
+          </>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// for all buttons of pagination
+// {pageCount>1 && new Array(pageCount).fill(0).map((item,index)=>{
+//   return(
+//     <button style={{border:"2px solid black"}} onClick={()=>setPage(index+1)} key={index}>{index+1}</button>
+//   )
+// })}
--- Products-with-pagination/App-with-pagination/src/components/Postitem.jsx
+++ Products-with-pagination/App-with-pagination/src/components/Postitem.jsx
@@ -1,15 +1,41 @@
-function Postitem({post}){
-
-    return(
-        <>{post.map((item)=>
-            
-            <div id="item" style={{borderRadius:"10px",textAlign:"left",padding:"15px",boxShadow: "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"}} key={item.id}>
-                {item.id%2==0?(<h2 style={{color:"red"}}>ID: {item.id}</h2>):(<h2 style={{color:"Green"}}>ID: {item.id}</h2>)}
-                <h2>Title: {item.title}</h2>
-                <h3>Body: {item.body}</h3>
-            </div>)}
-            </>
-    )
+function Postitem({ post }) {
+  return (
+    <>
+      {post.map((item) => (
+        <div
+          id="item"
+          style={{
+            // height: "320px",
+            overflowY: "hidden",
+            borderRadius: "10px",
+            textAlign: "left",
+            padding: "15px",
+            boxShadow:
+              "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px",
+          }}
+          key={item.id}
+        >
+          {item.id % 2 == 0 ? (
+            <h2 style={{ color: "red" }}><span className="First-word">ID:</span> {item.id}</h2>
+          ) : (
+            <h2 style={{ color: "Green" }}><span className="First-word">ID:</span>  {item.id}</h2>
+          )}
+          <h2><span className="First-word">Title:</span>  {item.title}</h2>
+          <h3
+            style={{
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              display: "-webkit-box",
+              WebkitLineClamp: 4,
+              WebkitBoxOrient: "vertical",
+            }}
+          >
+            <span className="First-word">Body:</span> <i>{item.body}</i>
+          </h3>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default Postitem
+export default Postitem;
